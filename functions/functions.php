1487a91b57f4e5d7a2942585a9ecd45f1fe61d3a
Lucas Delobelle <lucas@ld-web.net>
2023-03-03 14:06:17 +0100

    updated functions

--- functions/functions.php
+++ functions/functions.php
@@ -1,26 +1,23 @@
 <?php
 function sum(int $x, int $y): int
 {
-    $result = $x + $y;
-    return $result;
+  $result = $x + $y;
+  return $result;
 }
 
 // Fonction qui prendra un texte en paramètre
 // et retournera le même texte encadré par des balises p
 // transformToParagraph
 // textToParagraph
-function textToParagraphOrDiv(string $text, string $tagName = "p"): string
+function textToParagraphOrDiv(string $text, string $tagName = 'p'): string
 {
-    $allowedTags = ['p', 'div'];
+  $allowedTags = ['p', 'div'];
 
-    if (!in_array($tagName, $allowedTags)) {
-        exit('La balise ' . $tagName . ' est interdite, balises autorisées : ' . implode(',', $allowedTags));
-    }
+  if (!in_array($tagName, $allowedTags)) {
+    exit('Balise non autorisée. Balises autorisées : ' . implode(',', $allowedTags));
+  }
 
-    $openingTag = '<' . $tagName . '>';
-    $closingTag = '</' . $tagName . '>';
-
-    return $openingTag . $text . $closingTag;
+  return "<$tagName>$text</$tagName>";
 }
 
 // Fonction qui prend des textes
@@ -28,16 +25,16 @@ function textToParagraphOrDiv(string $text, string $tagName = "p"): string
 // séparés par des balises <br />
 function combineTextsWithBrTags(string ...$texts): string
 {
-    // return implode("<br />", $texts);
+  // return implode("<br />", $texts);
 
-    $result = '';
+  $result = '';
 
-    for ($i = 0; $i < count($texts); $i++) {
-        $result .= $texts[$i];
-        if ($i < count($texts) - 1) {
-            $result .= "<br />";
-        }
+  for ($i = 0; $i < count($texts); $i++) {
+    $result .= $texts[$i];
+    if ($i < count($texts) - 1) {
+      $result .= "<br />";
     }
+  }
 
-    return $result;
+  return $result;
 }
